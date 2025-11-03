3f242394b77b0e984dba5b69320ec2d46ceb4189
Abdullah-426 <abdullah.ansari3101@gmail.com>
2025-11-04 02:14:27 +0530

    logger update

--- medical-api-endpoints/app/logging_conf.py
+++ medical-api-endpoints/app/logging_conf.py
@@ -1,4 +1,12 @@
 from loguru import logger
 import sys
+from app.config import settings
+
 logger.remove()
-logger.add(sys.stderr, level="INFO", enqueue=True, backtrace=True, diagnose=False)
+logger.add(
+    sys.stderr,
+    level=settings.LOG_LEVEL or "INFO",
+    enqueue=True,
+    backtrace=True,
+    diagnose=False,
+)
