ddabad00f6c7338041ea5beb973e932278401447
Abdullah-426 <abdullah.ansari3101@gmail.com>
2025-11-03 16:30:07 +0530

    updated hero section and button radius

--- Frontend/swaasthya-saathi-frontend/src/App.tsx
+++ Frontend/swaasthya-saathi-frontend/src/App.tsx
@@ -1,12 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
+
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
